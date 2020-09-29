df9648781de95e4a98b20d4de102a93763d49315
Jesús Moreno Montiel <jesus.moreno.montiel@gmail.com>
2020-09-29 05:04:39 -0700

    #NA:Dummy strapi

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import axios from 'axios'
 
-const Home = ({ employees, error }) => {
+const Home = ({ restaurants, error }) => {
   if (error) {
     return <div>An error occured: {error.message}</div>
   }
   return (
     <ul>
-      {employees.map(employee => <li id={employee.id} > {employee.employee_name}</li>)}
+      {restaurants.map(restaurant => <li key={restaurant.id}>{restaurant.name}</li>)}
     </ul>
   )
 }
 
 Home.getInitialProps = async (ctx) => {
   try {
-    const res = await axios.get('http://dummy.restapiexample.com/api/v1/employees')
-    const employees= res.data
-    return { employees}
+    const res = await axios.get('https://www.3e9n5swz6ih-unjv7qa-loktbznhzmqog.eu-4.platformsh.site/restaurants')
+    const restaurants = res.data
+    return { restaurants }
   } catch (error) {
-    return { error }  
+    return { error }
   }
 }
 
