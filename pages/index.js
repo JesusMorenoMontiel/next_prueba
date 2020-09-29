7eadfde98da842c8b84d2ead2c5626b7cf57e170
Jesús Moreno Montiel <jesus.moreno.montiel@gmail.com>
2020-09-29 04:15:38 -0700

    #NA:Change API

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,24 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import axios from 'axios'
 
-const Home = ({ restaurants, error }) => {
+const Home = ({ employees, error }) => {
   if (error) {
     return <div>An error occured: {error.message}</div>
   }
   return (
     <ul>
-      {restaurants.map(restaurant => <li id={restaurant.id} > {restaurant.Name}</li>)}
+      {employees.map(employee => <li id={employee.id} > {employee.employee_name}</li>)}
     </ul>
   )
 }
 
 Home.getInitialProps = async (ctx) => {
   try {
-    const res = await axios.get('http://localhost:1337/restaurants')
-    const restaurants = res.data
-    return { restaurants }
+    const res = await axios.get('http://dummy.restapiexample.com/api/v1/employees')
+    const employees= res.data
+    return { employees}
   } catch (error) {
-    return { error }
+    return { error } 
   }
 }
 
